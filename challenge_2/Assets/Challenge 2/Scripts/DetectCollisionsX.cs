59681aad86b5387d2cb793a3e4b853867df74578
wennnny <ashley900121@gmail.com>
2024-04-11 19:49:46 +0800

    push chellenge_2 & Tutorial_3.

---
+++ challenge_2/Assets/Challenge 2/Scripts/DetectCollisionsX.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollisionsX : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collided with: " + gameObject.name);
+        Destroy(gameObject);
+    }
+}
---
+++ challenge_2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float timeBetweenDogs = 0.2f;
+    private float timeSinceLastDog = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeSinceLastDog += Time.deltaTime;
+
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && timeSinceLastDog > timeBetweenDogs)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            timeSinceLastDog = 0f;
+        }
+    }
+}
